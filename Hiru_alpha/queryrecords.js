45b4929ac2e7b1485dfaf0a9ac8ddbb7393d38b5
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-01 16:03:58 +0530

    query

---
+++ Hiru_alpha/queryrecords.js
@@ -0,0 +1,21 @@
+let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
+
+	ddb.query({
+		TableName: 'ThuvvaTable',
+		ExpressionAttributeValues: { ':test': '002' },
+		KeyConditionExpression: 'ID = :test',
+		FilterExpression: '',
+	}, function (err, data) {
+		if (err) {
+			console.log('error : ', err);
+		} else {
+			console.log('data : ', data);
+			callback(null, data);
+		}
+	});
+
+
+	callback(null, 'Successfully executed');
+}
