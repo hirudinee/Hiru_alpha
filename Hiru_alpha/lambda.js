ab45b34ec684e9bf72008f17e90155a194ba4be5
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-01 15:30:49 +0530

    api added - DB added

--- Hiru_alpha/lambda.js
+++ Hiru_alpha/lambda.js
@@ -1,16 +1,29 @@
 let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
+
 	ddb.put({
 		TableName: 'ThuvvaTable',
 		Item: { 'ID': '001', 'Name': 'hiru' }
 	}, function (err, data) {
 		if (err) {
-			console.log('error : ',err);
+			console.log('error : ', err);
 		} else {
-			console.log('data : ',data);
+			console.log('data : ', data);
 		}
 	});
+
+	ddb.delete({
+		TableName: 'ThuvvaTable',
+		Key: { 'ID': '001' }
+	}, function (err, data) {
+		if (err) {
+			console.log('error : ', err);
+		} else {
+			console.log('data : ', data);
+		}
+	});
+
 
 
 	callback(null, 'Successfully executed');
