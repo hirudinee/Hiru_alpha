a376fbdb613c1ab367b1a568c65934071582a522
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-01 15:40:11 +0530

    put data

--- Hiru_alpha/lambda.js
+++ Hiru_alpha/lambda.js
@@ -2,9 +2,13 @@ let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
 
+	var insert={};
+	insert.id =String(event.id);
+	insert.name=String(event.name);
+
 	ddb.put({
 		TableName: 'ThuvvaTable',
-		Item: { 'ID': '001', 'Name': 'hiru' }
+		Item: { 'ID': insert.id, 'Name': insert.name }
 	}, function (err, data) {
 		if (err) {
 			console.log('error : ', err);
