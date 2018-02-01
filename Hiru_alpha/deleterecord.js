c822838f18caa521146ba45b0f5b38abdb759597
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-01 15:37:02 +0530

    delete DB

--- Hiru_alpha/deleterecord.js
+++ Hiru_alpha/deleterecord.js
@@ -1,10 +1,10 @@
 let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
-
-	ddb.put({
+	
+	ddb.delete({
 		TableName: 'ThuvvaTable',
-		Item: { 'ID': '001', 'Name': 'hiru' }
+		Key: { 'ID': 'ID' }
 	}, function (err, data) {
 		if (err) {
 			console.log('error : ', err);
